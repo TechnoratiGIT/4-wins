f6db4c4054fe663df462764fabde9ffb2658ba50
TechnoratiGIT <94466773+TechnoratiGIT@users.noreply.github.com>
2021-11-16 17:41:37 +0100

    Update file_support.py

--- file_support.py
+++ file_support.py
@@ -18,31 +18,3 @@ def get_content(path, print_content=False):
 def save_content(path, content):
     with open(path, "w+") as f:
         f.write(json.dumps(content))
-
-
-def save_list(path, content: list):
-    with open(path, "w+") as f:
-        f.write("[\n")
-        extra = ",\n"
-        for i in range(len(content)):
-            if i == len(content) - 1:
-                extra = "\n"
-            f.write(str(content[i]) + extra)
-        f.write("]")
-
-
-def list_to_save_string(list_: list, st=False):
-    s = "\n"
-    extra = ",\n"
-    for i in range(len(list_)):
-        if i == len(list_) - 1:
-            extra = "\n"
-        if isinstance(list_[i], str):
-            a = "\"" + list_[i] + "\"" + extra
-        else:
-            a = str(list_[i]) + extra
-        s += a
-    if not st:
-        return [s]
-    else:
-        return "[" + s + "]"
